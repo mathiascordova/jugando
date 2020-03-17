b96f68c3b01016a40bc461973e8d02afab2c0731
mathiascordova <macordova@miuandes.cl>
2020-03-17 15:14:49 -0300

    first commit

---
+++ nombre/nombre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
